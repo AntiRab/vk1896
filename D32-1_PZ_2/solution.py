e756695f5433e27996461b52ba788d8f354b164c
Andrey <rab.helios@gmail.com>
2025-03-11 18:21:31 +0300

    Добавлен D32-1_pz_2

---
+++ D32-1_PZ_2/solution.py
@@ -0,0 +1,14 @@
+"""
+Необходимо написать программу, которая будет считывать со стандартного
+ввода название города и время и выводить информацию в формате
+“Current location is [Location] and time is [Time]”
+Пример 1
+    Входные данные
+        Moscow 12:00
+    Выходные данные
+        Current location is Moscow and time is 12:00
+"""
+
+city = input()
+time = input()
+print("Current location is", city, "and time is", time)
